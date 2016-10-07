45cf1b614cc18c98556b597eea867e7b20d25c91
ModalSeoul <Algernon@teknik.io>
2016-10-07 15:07:29 -0600

    herp i am rly smart

--- WeebFM/settings.py
+++ WeebFM/settings.py
@@ -50,9 +50,6 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     )
