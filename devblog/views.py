8e9d3317515613f1e8d3f428fc70d458a8d9cfb9
ModalSeoul <Algernon@teknik.io>
2016-10-16 02:31:01 -0600

    Staff only posting on Devblogs. Also devblogs fixed.

--- devblog/views.py
+++ devblog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.response import Response
 from devblog.models import Entry
 from devblog.serializers import EntrySerializer
 
@@ -7,3 +8,16 @@ from devblog.serializers import EntrySerializer
 class EntryView(viewsets.ModelViewSet):
     queryset = Entry.objects.all()
     serializer_class = EntrySerializer
+
+    def create(self, request):
+        data = self.request.data # aliasing
+        user = self.request.user
+        if user.is_staff:
+            entry = Entry.objects.create(
+                title=data.get('title'),
+                body=data.get('body'),
+                author=user)
+            serializer = EntrySerializer(entry)
+            return Response(serializer.data)
+        else:
+            return Response('You\'re not fucking staff.')
