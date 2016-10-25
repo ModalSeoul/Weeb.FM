9fb24f9a3c9345d76993bd483b9b700391fe1fdb
shakedown-street <pricejordan@outlook.com>
2016-10-24 19:01:05 -0600

    Closes #23, combined the ways to scrobble and scrobbler downloads lists into a table.

--- NewFrontend/app/src/app/components/start/start.component.ts
+++ NewFrontend/app/src/app/components/start/start.component.ts
@@ -8,12 +8,36 @@ import { AppComponent } from '../../app.component';
 })
 export class StartComponent implements OnInit {
 
+  public scrobblers : any;
+
   constructor(
     private app: AppComponent
   ) { }
 
   ngOnInit() {
     this.app.loading = false;
+    this.scrobblers = [
+      {
+        name: 'Chrome Extension (Bandcamp, Google Play, Pandora)',
+        link: 'https://github.com/ModalSeoul/Weeb-Chrome-Scrobbler'
+      },
+      {
+        name: 'Foobar2000',
+        link: '/static/Foobar.exe'
+      },
+      {
+        name: 'MPD',
+        link: 'https://github.com/ModalSeoul/mpd-scrobbler'
+      },
+      {
+        name: 'MusicBee',
+        link: '/static/MusicBee.exe'
+      },
+      {
+        name: 'RipFM (From Last.fm to Wilt)',
+        link: '/static/ripfm.exe'
+      }
+    ];
   }
 
 }
