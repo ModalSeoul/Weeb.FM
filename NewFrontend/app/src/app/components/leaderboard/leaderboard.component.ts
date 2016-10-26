1e5ca4a3abc60f0ce1c4e9b86d17b374d4ac30e1
shakedown-street <pricejordan@outlook.com>
2016-10-25 22:49:14 -0600

    Cleaned Leaderboard component.

--- NewFrontend/app/src/app/components/leaderboard/leaderboard.component.ts
+++ NewFrontend/app/src/app/components/leaderboard/leaderboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ScrobbleService, UserService } from '../../services/index';
+import { UserService } from '../../services/index';
 import { AppComponent } from '../../app.component';
 
 @Component({
@@ -8,11 +8,9 @@ import { AppComponent } from '../../app.component';
   styleUrls: ['./leaderboard.component.scss']
 })
 export class LeaderboardComponent implements OnInit {
-  private scrobbles: Array<any> = [];
   private topUsers: Array<any> = [];
 
   constructor(
-    // private Scrobbles: ScrobbleService,
     private users: UserService,
     private app: AppComponent
   ) { }
@@ -20,14 +18,8 @@ export class LeaderboardComponent implements OnInit {
   ngOnInit() {
     this.users.getLeaderboardUsers().subscribe((r : any) => {
       this.topUsers = r;
-      console.log(this.topUsers);
       this.app.loading = false;
     });
-
-    // this.Scrobbles.getRecent(50).subscribe((r: any) => {
-    //   this.scrobbles = r;
-    //   this.app.loading =  false;
-    // });
   }
 
 }
