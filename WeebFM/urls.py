948f24ee343e45de9b53320ecb26b2fc90b6f987
ModalSeoul <algernon@teknik.io>
2016-11-03 17:10:18 -0700

    going home

--- WeebFM/urls.py
+++ WeebFM/urls.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import url, include
 from django.conf.urls.static import static
